468e22019cba14b0c9c631fcf5dc731a535619f2
Hugo Martinez <manya01@protonmail.com>
2019-06-18 16:10:53 -0400

    nameLike-pageable

--- school/src/main/java/com/lambdaschool/school/service/StudentService.java
+++ school/src/main/java/com/lambdaschool/school/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.school.service;
 
 import com.lambdaschool.school.model.Student;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,7 +11,7 @@ public interface StudentService
 
     Student findStudentById(long id);
 
-    List<Student> findStudentByNameLike(String name);
+    List<Student> findStudentByNameLike(String name, Pageable pageable);
 
     void delete(long id);
 
